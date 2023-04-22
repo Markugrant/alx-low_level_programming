f64e3bd3eaad6d0f3fbeb5a17d44ec087d3aaa10
Markugrant <kathryndg19@gmail.com>
2023-04-22 06:55:47 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,18 +12,20 @@ int main(void)
 
 	for (a = 48; a < 58; a++)
 	{
-		putchar(a);
 	for (b = 49; b < 58; b++)
 	{
-		putchar(b);
 	for (c = 50; c < 58; c++)
 	{
+	if (c > b && b > a)
+	{
+		putchar(a);
+		putchar(b);
 		putchar(c);
-
 	if (a != 55 || b != 56)
 	{
 		putchar(',');
 		putchar(' ');
+	}
 	}
 	}
 	}
