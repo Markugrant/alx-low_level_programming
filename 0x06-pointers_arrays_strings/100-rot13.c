8db95391c84bdf8171fb22f6eb9e8523b1366824
Markugrant <kathryndg19@gmail.com>
2023-05-04 14:56:46 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rot13 - encoder rot13
+ * @s: pointer to string params
+ * Return: *s
+ */
+char *rot13(char *s)
+{
+	int a;
+	int b;
+
+	char *x = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *y = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (b = 0; x[b] != '\0'; b++)
+		{
+			if (s[a] == x[b])
+			{
+				s[a] = y[b];
+				break;
+			}
+		}
+	}
+	return (s);
+
+}
